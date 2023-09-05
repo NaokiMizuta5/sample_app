cad22065bf42c4451ea9addc09b218acb7771f2e
NaokiMizuta5 <naoki.mizuta.work@gmail.com>
2023-09-05 05:23:15 +0000

    [FINISH] CRUD実装済み

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,9 +4,15 @@ class ListsController < ApplicationController
   end
   
   def create 
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    
+  # エラーメッセージを扱うときにはrender, それ以外はredirect_to
+    if @list.save
+      redirect_to list_path(list.id)
+    else
+      render :new
+  # ここでredirect_toを使うと@listがnewアクション中でリセットされ、エラーを取得できない
+    end
   end
   
   def index
@@ -33,11 +39,17 @@ class ListsController < ApplicationController
   
   end
   
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
+  
   # 以下の関数はこのコントローラーからからしか呼び出せない、またアクションとして認識されない
   private 
   # ストロングパラメータ:攻撃を防ぐため、保存できるデータのモデルとカラムを絞る
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
     # paramsはformで送信されたデータが格納されている
     # requireで指定のモデルに対応するデータ(この場合title, body)のみに
     # permitで保存できるカラムを指定
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,16 @@ Rails.application.routes.draw do
   # get 'homes/top'はget 'homes/top' => 'homes#top'の省略形
   # すなわちパスhomes/topにアクセスするとコントローラーHomesのtopアクションが行われる
   get '/top' => 'homes#top'
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  # :id はlists/:id = lists/1 とかlists/2に該当
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  # get 'lists/new'
+  # post 'lists' => 'lists#create'
+  # get 'lists' => 'lists#index'
+  # # :id はlists/:id = lists/1 とかlists/2に該当
+  # get 'lists/:id' => 'lists#show', as: 'list'
+  # get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  # patch 'lists/:id' => 'lists#update', as: 'update_list'
+  # delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+  resources :lists
+  # コメントアウトした部分は一般的なので上の一行で自動生成される
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
